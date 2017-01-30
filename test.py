e1a04ffa61777e3aea34ad4d0861effecfa93e3e
Chad Halvorsen <halvorsenca@appstate.edu>
2017-01-30 17:37:11 -0500

    got links in table

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-page = urlopen('http://www.icy-veins.com/hearthstone/')
-soup = BeautifulSoup(page)
+page = urlopen('http://www.icy-veins.com/hearthstone/warrior-standard-decks')
+soup = BeautifulSoup(page,'html.parser')
 
-print(soup.prettify())
+all_tables=soup.find('table')
+decks = soup.find('table', class_='deck_presentation')
+
+links = decks.findAll('a')
+for link in links:
+    print(link.get("href"))
+
+#warrior_links = decks.find_all("a")
+#for link in warrior_links:
+#    print(link.get("href"))
