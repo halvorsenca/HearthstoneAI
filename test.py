ffa3ef29f0457ca53f31fc786cfeebf99bec0c3b
Chad Halvorsen <halvorsenca@appstate.edu>
2017-02-06 18:26:44 -0500

    still working

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import sys
+#from pymongo import MongoClient
+
+#client = MongoClient()
+#db = client.test
+
+#sys.stdout = open("cards.txt","w")
+
 
 page = urlopen('http://www.icy-veins.com/hearthstone/warrior-standard-decks')
 soup = BeautifulSoup(page,'html.parser')
@@ -7,6 +15,8 @@ soup = BeautifulSoup(page,'html.parser')
 deck = soup.find(class_="deck_presentation_name")
 
 decks = soup.find_all(class_="deck_presentation_name")
+
+decknum = 1;
 
 for link in decks:
     line = link.a
@@ -18,7 +28,21 @@ for link in decks:
     cardTable = newSoup.find('table', class_="deck_card_list")
     entry = cardTable.find_all('li')
     for card in entry:
-        print(card.contents[0])
-        print(card.contents[1].contents[0])
+        amount = int(card.contents[0][0])        
+        name = card.contents[1].contents[0]
+        
+        for x in range(0, amount):
+            result = db.decks.insert_one(
+                {
+                    "deckID": str(decknum),
+                }
+            
 
+
+
+
+#resultHero = db.heroes.insert_one(
+#    {
+#        #heroname: [deckID1, deckID2, ...]
+#    }
 
