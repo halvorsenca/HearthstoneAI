e027bb48b9414b8b0f43620a6f86e866ea4d6eb0
Chad Halvorsen <halvorsenca@appstate.edu>
2017-02-06 17:20:20 -0500

    got it for warrior

--- test.py
+++ test.py
@@ -4,13 +4,21 @@ from urllib.request import urlopen
 page = urlopen('http://www.icy-veins.com/hearthstone/warrior-standard-decks')
 soup = BeautifulSoup(page,'html.parser')
 
+deck = soup.find(class_="deck_presentation_name")
+
 decks = soup.find_all(class_="deck_presentation_name")
 
 for link in decks:
     line = link.a
-    print(line['href'])
+    if any(word in line['href'] for word in ("cheap", "budget", "basic")):
+        continue
+    newPage = urlopen("http:" + line['href'])
+    newSoup = BeautifulSoup(newPage,'html.parser')
 
-#for link in decks:
-#    links = decks.find_all("href")
+    cardTable = newSoup.find('table', class_="deck_card_list")
+    entry = cardTable.find_all('li')
+    for card in entry:
+        print(card.contents[0])
+        print(card.contents[1].contents[0])
 
-#print(links)
+
