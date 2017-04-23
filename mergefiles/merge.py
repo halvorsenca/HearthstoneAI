401a6370180c807fdd7dc6744a43ce84fa770884
camwood23 <camwood23@gmail.com>
2017-04-23 18:10:18 -0400

    Fixed merge.py

--- mergefiles/merge.py
+++ mergefiles/merge.py
@@ -2,22 +2,31 @@ from os import listdir
 from ast import literal_eval
 import json
 
+##
+# Please make sure Files have either StateQualitiesX or VisitedX
+# somewhere in the filename, where X is a unique number
+##
+
 files = listdir('Files')
 
 ThreadQualities = []
 ThreadVisited = []
-for f in files:
-	with open('Files/'+f, 'r') as infile:
-		tmp = json.load(infile)
-		ThreadQualities.append({})
-		for key, value in tmp.items():
-			ThreadQualities[-1][literal_eval(key)] = value
 
-	with open('Files/'+f, 'r') as infile:
-		tmp = json.load(infile)
-		ThreadVisited.append({})
-		for key, value in tmp.items():
-			ThreadVisited[-1][literal_eval(key)] = value
+for _ in range(0, int(len(files)/2)):
+	ThreadQualities.append({})
+	ThreadVisited.append({})
+
+for f in files:
+	if "StateQualities" in f:
+		with open('Files/'+f, 'r') as infile:
+			tmp = json.load(infile)
+			for key, value in tmp.items():
+				ThreadQualities[int(''.join([x for x in f if x.isdigit()]))][literal_eval(key)] = value
+	elif "Visited" in f:
+		with open('Files/'+f, 'r') as infile:
+			tmp = json.load(infile)
+			for key, value in tmp.items():
+				ThreadVisited[int(''.join([x for x in f if x.isdigit()]))][literal_eval(key)] = value
 
 ## Average together all the values output from threads
 StateQualities = {}
@@ -29,7 +38,7 @@ for _ in range(0, len(files)):
 	loopQualities = ThreadQualities.pop(0)
 	loopVisited = ThreadVisited.pop(0)
 	for key, value in loopQualities.items():
-		if value == 0:
+		if value == 0 or not key[0] in loopVisited.keys():
 			continue
 		for i in range(0, len(ThreadQualities)):
 			if key in ThreadQualities[i].keys():
@@ -37,7 +46,7 @@ for _ in range(0, len(files)):
 				if x != 0:
 					q.append(x)
 					v.append(ThreadVisited[i][key[0]])
-		totalV= sum(v) + loopVisited[key[0]]
+		totalV = sum(v) + loopVisited[key[0]]
 		avgQ = value * (loopVisited[key[0]] / totalV)
 		for i in range(0, len(q)):
 			avgQ += q[i] * (v[i] / totalV)
